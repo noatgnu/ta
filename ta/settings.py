b9cacc301b4a2b0f1803e70392adb6e11e806f05
Toan Phung <toan.phung@proteo.info>
2023-11-20 11:20:24 +0000

    update

--- ta/settings.py
+++ ta/settings.py
@@ -197,11 +197,11 @@ REDIS_HOST = os.environ.get("REDIS_HOST", "taredis")
 REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
 REDIS_DB = os.environ.get("REDIS_DB", "0")
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "redis")
-REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}"
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": REDIS_URL
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
+#         "LOCATION": REDIS_URL
+#     }
+# }
